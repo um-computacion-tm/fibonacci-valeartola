d0428d4d1bae4089db0196b93883576387aea1b5
Valentina Artola <valentinaartola@pop-os.localdomain>
2023-05-01 18:55:53 -0300

    Fibonacci

---
+++ Fibonacci.py
@@ -0,0 +1,25 @@
+import unittest
+
+#forma iterativa
+def fibonacci(number):
+    fibonacci_numbers = [0, 1]
+    total = 0
+    print("number " + str(number))
+    for index in range(2, number+1):
+        total += fibonacci_numbers[-2] + fibonacci_numbers[-1]
+        fibonacci_numbers.append(total)
+        print("fibonacci_numbers " + str(fibonacci_numbers))
+        print("total " + str(total))
+    return total
+
+#forma recursiva
+def fibonacci(number):
+    if number <= 1:
+        return number
+    return fibonacci(number - 2) + fibonacci(number - 1)
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
